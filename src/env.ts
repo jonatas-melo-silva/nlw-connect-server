ec92de075616912157384dd48e5f8d2c9e646435
Jonatas Melo Silva <jonatas891024@gmail.com>
2025-02-21 14:55:44 -0300

    feat: adiciona integracao com postegres e redis

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,10 @@ import z from 'zod'
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string(),
+  REDIS_URL: z.string().url(),
+  API_URL: z.string().url(),
+  WEB_URL: z.string().url(),
 })
 
 export type Env = z.infer<typeof envSchema>
